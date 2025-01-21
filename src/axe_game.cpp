0a5c4d04caff956be2aadbf10b09ad0d7240713e
Dejan <dekipopica92@gmail.com>
2025-01-21 14:15:08 +0100

    - Moving circle in 2 different directions using A nd D key

--- src/axe_game.cpp
+++ src/axe_game.cpp
@@ -8,16 +8,43 @@ void print(std::string Message)
 
 int main()
 {
-    int width  = 350;
-    int height = 200;
-    InitWindow(width,height,"AXE GAME!");   
+    // Window dimensions
+    int width  {350};
+    int height {200};
+    InitWindow(width,height,"AXE GAME!");
+
+    //Circle coordinates
+    int circle_x {175};
+    int circle_y {100};
+    int circle_r {25};
+
+    SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(RED);
-        // Circle is in the niddle of the screen
-        DrawCircle(175,100,25,BLUE);
+        ClearBackground(WHITE);
+        
+        /*Game Logic begins here*/
+
+        // Circle is in the middle of the screen
+        DrawCircle(circle_x,circle_y,circle_r,BLUE);
+
+        if (IsKeyDown(KEY_D))
+        {
+            // Circle moves by 10 pixel if D is pressed
+            circle_x += 10;
+        }
+
+        if (IsKeyDown(KEY_A))
+        {
+            // Circle moves by 10 pixel if D is pressed
+            circle_x =- 10;
+        }
+
+
+         /*Game Logic ends here*/
+
         EndDrawing();
     }
 }
