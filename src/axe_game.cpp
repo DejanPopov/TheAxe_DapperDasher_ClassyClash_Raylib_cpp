fed408728dde9e32ebdcda2765deda0ab4725df4
Dejan <dekipopica92@gmail.com>
2025-01-21 15:32:01 +0100

    - Collison calculation

--- src/axe_game.cpp
+++ src/axe_game.cpp
@@ -19,9 +19,23 @@ int main()
     int circle_y {200};
     int circle_r {25};
 
+    // Circle edges
+    int l_circle_x {circle_x - circle_r}; // X coordinate of the circle left edge
+    int r_circle_x {circle_x + circle_r}; // X coordinate of the circle right edge
+    int u_circle_y {circle_y - circle_r}; // Y coordinate of the circle upper edge
+    int b_circle_y {circle_y + circle_r}; // Y coordinate of the circle bottom edge
+
     // Axe coordinates
     int axe_x     {400};
     int axe_y     {0};
+    int axe_len   {50};
+
+    // Axe edges
+    int l_axe_x {axe_x};
+    int r_axe_x {axe_x + axe_len};
+    int u_axe_y {axe_y};
+    int b_axe_y {axe_y + axe_len};
+
     int direction {10};
     
     while (!WindowShouldClose())
@@ -33,19 +47,19 @@ int main()
 
         // Circle is in the middle of the screen
         DrawCircle(circle_x,circle_y,circle_r,BLUE);
-        DrawRectangle(axe_x,axe_y,50,50,RED);
+        DrawRectangle(axe_x,axe_y,axe_len,axe_len,RED);
 
         // Move the Axe
         // direction = -direction; will reverse to opposite direction
         axe_y += direction;
 
-        if (axe_y > 450 || axe_y < 0)
+        if (axe_y > height || axe_y < 0)
         {
             direction = -direction;
         }
 
         // Move the Circle
-        if (IsKeyDown(KEY_D) && circle_x < 350)
+        if (IsKeyDown(KEY_D) && circle_x < width)
         {
             // Circle moves by 10 pixel if D is pressed
             circle_x += 10;
